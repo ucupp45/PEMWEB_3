c8d12f8ee423a074493b62ba9e918c2f3de95a27
Fadilah <sitiraudatuljannahfadilah@gmail.com>
2025-01-05 09:55:18 +0700

    konsul ortu

--- resources/views/guru/dash_guru.blade.php
+++ resources/views/guru/dash_guru.blade.php
@@ -290,7 +290,7 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
-                        <h3>Data Konsultasi</h3>
+                        <h3>Data Konsultasi Orang Tua</h3>
                     </div>
                     <div class="card-body">
                         <div class="table-responsive">
